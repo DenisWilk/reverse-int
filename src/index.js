910481d6111ee0c2ca7028de914af9493c1ff74a
Denis V <info.d1@tut.by>
2022-02-25 02:22:59 +0300

    feat: add solution to index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
+
+    const num = Math.abs(n);
+    const strNum = num.toString();
+    const arrRevNum = Array.from(strNum).reverse();
+    const strFromArr = arrRevNum.join('');
+    const res = Number(strFromArr);
   
-}
+    return res;
+  }
